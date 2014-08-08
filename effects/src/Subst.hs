7f2538eada7275847c58fb076846323f3075e95d
Iago Abal <mail@iagoabal.eu>
2014-08-08 14:44:36 +0200

    Effects: fix substitution

--- effects/src/Subst.hs
+++ effects/src/Subst.hs
@@ -1,5 +1,5 @@
 
--- | One-shot subsitution for type variables
+-- | Substitution for type-level variables
 module Subst where
 
 import           Data.Map.Strict ( Map )
@@ -27,18 +27,32 @@ fromList ps = Subst rr ee tt
         tt = Map.fromList [ (v,VarTy w)  | (v,w) <- ps, isTypeVar v ]
 
 var2region :: TyVar -> Region -> Subst
-var2region y p = Subst (Map.singleton y p) Map.empty Map.empty
+var2region y p@(VarReg _)
+  | VarReg y /= p = Subst (Map.singleton y p) Map.empty Map.empty
+  | otherwise     = Subst.id
+var2region _ _ = error "var2region: mapping to a non-variable"
 
 var2effect :: TyVar -> Effect -> Subst
-var2effect f s = Subst Map.empty (Map.singleton f s) Map.empty
+var2effect f s
+  | VarEff f /= s = Subst Map.empty (Map.singleton f s) Map.empty
+  | otherwise     = Subst.id
 
 var2type :: TyVar -> Type -> Subst
-var2type a t = Subst Map.empty Map.empty (Map.singleton a t)
+var2type a t
+  | VarTy a /= t = Subst Map.empty Map.empty (Map.singleton a t)
+  | otherwise    = Subst.id
+
+-- cleanup :: Subst -> Subst
+-- cleanup (Subst rr ee tt) = Subst rr' ee' tt'
+--   where rr' = Map.filterWithKey (\k x -> x /= VarReg k) rr
+--         ee' = Map.filterWithKey (\k x -> x /= VarEff k) ee
+--         tt' = Map.filterWithKey (\k x -> x /= VarTy k) tt
 
 infixl 7 ++.
 (++.) :: Subst -> Subst -> Subst
-(Subst r1 e1 t1) ++. (Subst r2 e2 t2) =
+u1@(Subst r1 e1 t1) ++. u2 =
   Subst (Map.union r1 r2) (Map.union e1 e2) (Map.union t1 t2)
+  where (Subst r2 e2 t2) = u1 $. u2
 
 (\\) :: Subst -> [TyVar] -> Subst
 (Subst rr ee tt) \\ vs = Subst (deleteVars rr) (deleteVars ee) (deleteVars tt)
@@ -54,11 +68,17 @@ class SubstTarget a where
 instance SubstTarget a => SubstTarget [a] where
   u $. xs = map (u $.) xs
 
+instance SubstTarget Subst where
+  u $. (Subst rr ee tt) = Subst rr' ee' tt'
+    where rr' = Map.map (u $.) rr
+          ee' = Map.map (u $.) ee
+          tt' = Map.map (u $.) tt
+
 instance SubstTarget Region where
   _ $. p@(Reg _) = p
   u $. p@(VarReg y)
     | Just p' <- Map.lookup y (regMap u)
-    = p'
+    = u $. p'
     | otherwise
     = p
 
@@ -66,7 +86,10 @@ instance SubstTarget Effect where
   _ $. s@EmptyEff = s
   u $. s@(VarEff f)
     | Just s' <- Map.lookup f (effMap u)
-    = s'
+    -- k2subst introduces circular subsitutions and thus we need to be
+    -- sure we break this circularity by removing the already substituted
+    -- variable from the domain of the substitution.
+    = (u \\ [f]) $. s'
     | otherwise
     = s
   u $. (InitEff p t) = InitEff (u $. p) (u $. t)
@@ -78,7 +101,7 @@ instance SubstTarget Type where
   _ $. UnitTy = UnitTy
   u $. t@(VarTy a)
     | Just t' <- Map.lookup a (typMap u)
-    = t'
+    = u $. t'
     | otherwise
     = t
   u $. (RefTy p t) = RefTy (u $. p) (u $. t)
